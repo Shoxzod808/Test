44cecaa1655303ae9369c73558fc1219f5a45fbd
Shoxzod808 <pkzoom94320@gmail.com>
2022-09-16 15:49:02 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+from imap_tools import MailBox, AND, A
+
+# Get date, subject and body len of all emails from INBOX folder
+with MailBox('imap.gmail.com').login('pkzoom94320@gmail.com', 'yomfkroicvfuujlo') as mailbox:
+    for msg in mailbox.fetch(A()):
+        print(msg.text)
+        print(msg)
